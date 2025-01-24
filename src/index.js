ab894ec00508edc31cc7efa0572983f123a18c23
Murka69 <dec20465.holod@yandex.ru>
2025-01-24 21:07:06 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { initialCards } from "./scripts/cards.js";
 import { renderCards, createCardElement } from "./scripts/card.js";
 import { openModal, closeModal, setupPopupCloseListeners } from "./scripts/modal.js";
 import { enableValidation, clearValidation } from "./scripts/validation.js";
-import { addNewCard, getCardList, getUserInfo, updateUserInfo, updateAvatar } from "./scripts/api.js";
+import { addNewCard, getCardList, getUser Info, updateUser Info, updateAvatar } from "./scripts/api.js";
 
 let userId = null;
 
@@ -50,7 +50,6 @@ const config = {
 
 enableValidation(config);
 
-
 function handleAvatarSubmit(evt) {
     evt.preventDefault();
     const popupButtonSafe = popupTypeAvatar.querySelector(".popup__button");
@@ -69,7 +68,7 @@ function handleAvatarSubmit(evt) {
 
 profileAvatar.addEventListener("click", () => {
     openModal(popupTypeAvatar);
-    clearValidation(profileAvatar,config);
+    clearValidation(profileAvatar, config);
     popupAvatarForm.reset();
 });
 
@@ -85,7 +84,7 @@ function openImagePopup(name, link) {
 function openEditProfilePopup() {
     editProfileNameInput.value = profileTitleElement.textContent;
     editProfileDescriptionInput.value = profileDescriptionElement.textContent;
-    clearValidation(formElementEditProfile,config);
+    clearValidation(formElementEditProfile, config);
     openModal(popupTypeEdit);
 }
 
@@ -95,6 +94,7 @@ formElementEditProfile.addEventListener("submit", (evt) => {
     const newAbout = editProfileDescriptionInput.value;
     const popupButtonSafe = popupTypeEdit.querySelector(".popup__button");
     popupButtonSafe.textContent = "Сохранение...";
+    
     updateUserInfo(newName, newAbout)
         .then((updatedUserData) => {
             profileTitleElement.textContent = updatedUserData.name;
@@ -122,89 +122,6 @@ formElementNewCard.addEventListener("submit", (evt) => {
         .then((cardData) => {
             const cardElement = createCardElement(cardData, openImagePopup, cardData.owner._id);
             cardsContent.prepend(cardElement);
-            const config = {
-    formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible",
-};
-
-enableValidation(config);
-
-
-function handleAvatarSubmit(evt) {
-    evt.preventDefault();
-    const popupButtonSafe = popupTypeAvatar.querySelector(".popup__button");
-    popupButtonSafe.textContent = "Сохранение...";
-
-    updateAvatar(popupAvatarInput.value)
-        .then((res) => {
-            profileAvatar.style.backgroundImage = `url(${res.avatar})`;
-            closeModal(popupTypeAvatar);
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-        .finally(() => (popupButtonSafe.textContent = "Сохранить"));
-}
-
-profileAvatar.addEventListener("click", () => {
-    openModal(popupTypeAvatar);
-    clearValidation(profileAvatar,config);
-    popupAvatarForm.reset();
-});
-
-popupAvatarForm.addEventListener("submit", handleAvatarSubmit);
-
-function openImagePopup(name, link) {
-    popupImageElement.src = link;
-    popupImageElement.alt = name;
-    popupCaptionElement.textContent = name;
-    openModal(popupImage);
-}
-
-function openEditProfilePopup() {
-    editProfileNameInput.value = profileTitleElement.textContent;
-    editProfileDescriptionInput.value = profileDescriptionElement.textContent;
-    clearValidation(formElementEditProfile,config);
-    openModal(popupTypeEdit);
-}
-
-formElementEditProfile.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const newName = editProfileNameInput.value;
-    const newAbout = editProfileDescriptionInput.value;
-    const popupButtonSafe = popupTypeEdit.querySelector(".popup__button");
-    popupButtonSafe.textContent = "Сохранение...";
-    updateUserInfo(newName, newAbout)
-        .then((updatedUserData) => {
-            profileTitleElement.textContent = updatedUserData.name;
-            profileDescriptionElement.textContent = updatedUserData.about;
-            closeModal(popupTypeEdit);
-        })
-        .catch((err) => {
-            console.error("Ошибка при обновлении данных профиля:", err);
-        })
-        .finally(() => (popupButtonSafe.textContent = "Сохранить"));
-});
-
-buttonProfileEdit.addEventListener("click", openEditProfilePopup);
-setupPopupCloseListeners();
-
-buttonNewCard.addEventListener("click", () => openModal(popupTypeNewCard));
-
-formElementNewCard.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const popupButtonSafe = popupTypeNewCard.querySelector(".popup__button");
-    popupButtonSafe.textContent = "Сохранение...";
-    const newCardName = cardNameInput.value;
-    const newCardLink = cardUrlInput.value;
-    addNewCard(newCardName, newCardLink)
-        .then((cardData) => {
-            const cardElement = createCardElement(cardData, openImagePopup, cardData.owner._id,);
-            cardsContent.prepend(cardElement);
             closeModal(popupTypeNewCard);
             formElementNewCard.reset();
         })
@@ -214,38 +131,18 @@ formElementNewCard.addEventListener("submit", (evt) => {
         .finally(() => (popupButtonSafe.textContent = "Сохранить"));
 });
 
-
 Promise.all([getCardList(), getUserInfo()])
     .then(([cards, userData]) => {
         userId = userData._id;
         profileTitleElement.textContent = userData.name;
         profileDescriptionElement.textContent = userData.about;
         profileAvatar.style.backgroundImage = `url(${userData.avatar})`;
-        renderCards(cards, cardsContent, openImagePopup, userId, deleteCard, likeCard);
+        renderCards(cards, cardsContent, openImagePopup, userId);
     })
     .catch((err) => {
         console.error(err);
     });
+    
+   
 
-            closeModal(popupTypeNewCard);
-            formElementNewCard.reset();
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-        .finally(() => (popupButtonSafe.textContent = "Сохранить"));
-});
-
-
-Promise.all([getCardList(), getUserInfo()])
-    .then(([cards, userData]) => {
-        userId = userData._id;
-        profileTitleElement.textContent = userData.name;
-        profileDescriptionElement.textContent = userData.about;
-        profileAvatar.style.backgroundImage = `url(${userData.avatar})`;
-        renderCards(cards, cardsContent, openImagePopup, userData._id,);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
 
